5def623cde04889820592b848d296f623664725e
rcpirate <dy@dymatoMac-mini.local>
2014-06-20 20:28:45 +0800

    增加msg.py settings.py文件

--- server.py
+++ server.py
@@ -1,8 +1,44 @@
-'''
-Created on 2014年6月20日
+#code:utf-8
 
-@author: dy
-'''
+from twisted.internet.protocol import Protocol
+from twisted.internet.protocol import Factory
+from twisted.internet import reactor
+
+from settings import *
+
+from msg import Message
+
+FACTORY = None
+
+class ChatProtocol(Protocol):
+    
+    def __init__(self,factory):
+        self.factory = factory
+        print '__init__'
+    
+    def dataReceived(self, data):
+        print '...dataReceived'
+    
+    def connectionLost(self, reason):
+        print '...connectionLost'
+    
+
+class ProtocolFactory(Factory):
+    
+    protocols = 0
+    
+    def __init__(self):
+        pass
+    
+    def buildProtocol(self):
+        
+        self.protocols += 1
+        
+        return ChatProtocol(self)
+
 
 if __name__ == '__main__':
-    pass
+    
+    FACTORY = ProtocolFactory()
+    reactor.listenTCP(PORT,FACTORY)
+    reactor.run()
---
+++ settings.py
@@ -0,0 +1,14 @@
+#code:utf-8
+
+PORT = 8123
+
+PROTOCOLS = 1000
+
+DATABASE = {
+            'ip':'127.0.0.1',
+            'username':'root',
+            'passport':'root'
+            }
+
+if __name__ == '__main__':
+    pass
